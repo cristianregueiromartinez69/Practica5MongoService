8f021b96ef368059a11ed53dabd2de6d3d375691
cristian <cregueiromartinez@danielcastelao.org>
2025-03-06 14:33:22 +0100

    servicio y rest controller para crear el grupo

---
+++ src/main/java/com/example/AD/mongoService/controller/MongoRestController.java
@@ -0,0 +1,31 @@
+package com.example.AD.mongoService.controller;
+
+import com.example.AD.mongoService.model.entity.Grupo;
+import com.example.AD.mongoService.service.MongoService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mongoService")
+public class MongoRestController {
+
+    public final MongoService mongoService;
+
+    public MongoRestController(MongoService mongoService) {
+        this.mongoService = mongoService;
+    }
+
+    @PostMapping("/crear")
+    public ResponseEntity<String> crearDocumentoMongo(@RequestBody Grupo grupo) {
+        try{
+            mongoService.crearGrupo(grupo);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.ok().body("Documento creado correctamente");
+    }
+
+}

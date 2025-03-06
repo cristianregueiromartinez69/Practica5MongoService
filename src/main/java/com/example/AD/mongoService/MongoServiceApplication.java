3f4883f2930637186aeb22385c98f57cd358c34d
cristian <cregueiromartinez@danielcastelao.org>
2025-03-06 14:02:04 +0100

    commit inicial mongoService

---
+++ src/main/java/com/example/AD/mongoService/MongoServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.AD.mongoService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class MongoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoServiceApplication.class, args);
+	}
+
+}

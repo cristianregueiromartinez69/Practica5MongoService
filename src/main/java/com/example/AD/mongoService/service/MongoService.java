5418f5ac7564a712df119beec04339afa13ca874
cristian <cregueiromartinez@danielcastelao.org>
2025-03-06 14:29:11 +0100

    metodo de crear grupo

--- src/main/java/com/example/AD/mongoService/service/MongoService.java
+++ src/main/java/com/example/AD/mongoService/service/MongoService.java
@@ -1,9 +1,22 @@
 package com.example.AD.mongoService.service;
 
+import com.example.AD.mongoService.model.entity.Grupo;
+import com.example.AD.mongoService.repository.AlbumRepository;
+import com.example.AD.mongoService.repository.GrupoRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MongoService {
 
+    public final GrupoRepository grupoRepository;
+    public final AlbumRepository albumRepository;
 
+    public MongoService(GrupoRepository grupoRepository, AlbumRepository albumRepository) {
+        this.grupoRepository = grupoRepository;
+        this.albumRepository = albumRepository;
+    }
+
+    public void crearGrupo(Grupo grupo){
+        grupoRepository.save(grupo);
+    }
 }

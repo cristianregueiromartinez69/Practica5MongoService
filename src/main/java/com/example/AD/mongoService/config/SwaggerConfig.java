c7b399f4599c78ac5066199fc8641b7cc3455c16
cristian <cregueiromartinez@danielcastelao.org>
2025-03-08 11:49:21 +0100

    configuraciones hechas, vamos a probarlas

--- src/main/java/com/example/AD/mongoService/config/SwaggerConfig.java
+++ src/main/java/com/example/AD/mongoService/config/SwaggerConfig.java
@@ -1,9 +1,26 @@
 package com.example.AD.mongoService.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${spring.application.version}")
+    private String version;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title(appName)
+                        .description("Tarea 5 de acceso a datos de mongoService")
+                        .version(version));
+    }
 
 }
